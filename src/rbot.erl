d8348f3fe16fa918b747914ab5ced8c15a048fac
tjm1518 <info@tmoor.es>
2021-02-12 17:47:21 +0000

    Adds comments, TODO: Parse dice rolls

--- src/rbot.erl
+++ src/rbot.erl
@@ -1,7 +1,7 @@
 -module(rbot).
 -behaviour(application).
 
--export([start/2, stop/1, init/0, loop/1]).
+-export([start/2, stop/1, init/0, loop/2]).
 -include("priv_keys.hrl").
 -on_load(init/0).
 
@@ -15,33 +15,49 @@ init() ->
 start(_Type, _Args) ->
   case get_token() of
     nil -> {error, "Could not refresh token"};
-    Token -> {ok, spawn(?MODULE, loop, [Token])}
+    Token -> {ok, spawn(?MODULE, loop, [Token, erlang:system_time(seconds)*0.0])}
   end.
 
 stop(_State) ->
   ok.
 
-loop(Token) ->
-  {Modhash, Messages} = get_messages(Token),
-  [process_message(Msg, Modhash) || {[_Kind, {<<"data">>, {Msg}}]} <- Messages], 
-  ok.
+loop(Token, Time) ->
+  Messages = get_messages(Token),
+  Unread = lists:takewhile(fun ({[_Kind, {<<"data">>, {Msg}}]}) -> proplists:get_value(<<"created_utc">>, Msg) >= Time end, Messages),
+  N_Time = hd([process_message(Msg, Token) || {[_Kind, {<<"data">>, {Msg}}]} <- Unread]++[Time]),
+  timer:sleep(10000),
+  io:fwrite("Time is: ~f~n", [N_Time]),
+  loop(get_token(), N_Time).
 
 get_messages(Token) ->
   {ok,{{"HTTP/1.1",200,"OK"},
        _Content, Result}} = httpc:request(get,
-                                          {"https://oauth.reddit.com/message/unread",
+                                          {"https://oauth.reddit.com/message/mentions.json",
                                            [{"Authorization", "bearer "++Token},
                                             {"User-Agent", "TJ ? Auto_TJ"}]}, [], []),
   {Json} = jiffy:decode(Result),
   {Data} = proplists:get_value(<<"data">>, Json),
-  Modhash = proplists:get_value(<<"modhash">>, Data),
-  Children = proplists:get_value(<<"children">>, Data),
-  {Modhash, Children}.
+  proplists:get_value(<<"children">>, Data).
 
-process_message(Msg_Proplist, Modhash) ->
-  M_Id = proplists:get_value(<<"id">>, Msg_Proplist),
+process_message(Msg_Proplist, Token) ->
+  timer:sleep(1000),
+  M_Name = proplists:get_value(<<"name">>, Msg_Proplist),
   M_Body = proplists:get_value(<<"body">>, Msg_Proplist),
-  ok.
+  Body = "api_type=json&return_rtjson=false&text=\"hello!\"&thing_id="++binary_to_list(M_Name),
+  {ok,
+   {{"HTTP/1.1",200,"OK"},
+    _Content,
+    Result}} = httpc:request(post,
+                {"https://oauth.reddit.com/api/comment",
+                 [{"Authorization", "bearer "++Token},
+                  {"User-Agent", "TJ ? Auto_TJ"}],
+                 "application/x-www-form-urlencoded",
+                 Body
+                 },
+                [{ssl,[]}],
+                []),
+  io:fwrite("Result:~p~n", [Result]),
+  proplists:get_value(<<"created_utc">>, Msg_Proplist).
 
 get_token() ->
   Body = "grant_type=refresh_token&refresh_token="++?REFRESH_TOKEN,
