196e95ac9c6f8edebb968ca27efb914181dc47c3
tjm1518 <info@tmoor.es>
2021-02-14 20:04:11 +0000

    Added readme etc.

--- src/rbot.erl
+++ src/rbot.erl
@@ -1,28 +1,27 @@
 -module(rbot).
 -behaviour(application).
 
--export([start/2, stop/1, init/0, loop/1]).
+-export([start/2, stop/1, start_loop/0, loop/1]).
 -include("priv_keys.hrl").
--on_load(init/0).
-
-init() ->
-  application:ensure_all_started(crypto),
-  application:ensure_all_started(public_key),
-  application:ensure_all_started(ssl),
-  application:ensure_all_started(inets),
-  ok.
 
 start(_Type, _Args) ->
-  case get_token() of
-    nil -> {error, "Could not refresh token"};
-    Token -> {ok, spawn(?MODULE, loop, [Token])}
-  end.
+  rbot_sup:start_link().
 
 stop(_State) ->
   spawn(application, start, [rbot]),
   ok.
 
+start_loop() ->
+  case get_token() of
+    nil -> {error, "Could not refresh token"};
+    Token -> 
+      io:fwrite("Got Token~n"),
+      {ok, spawn(?MODULE, loop, [Token])}
+  end.
+
+
 loop(Token) ->
+  io:fwrite("checking inbox~n"),
   Messages = get_messages(Token),
   [process_message(Msg, Token) || {[_Kind, {<<"data">>, {Msg}}]} <- Messages],
   timer:sleep(10000),
@@ -45,17 +44,14 @@ process_message(Msg_Proplist, Token) ->
   case re:run(M_Body, "\\[\\d+d\\d+\\]", [{capture, all, list}, global]) of
     nomatch -> read_message(M_Name, Token);
     {match, Rolls} ->
-      Result = respond_message(Rolls, M_Name, Token),
-      case jiffy:decode(Result) of
-        {<<"json">>, {<<"errors">>, [], _}} -> read_message(M_Name, Token);
-        _ -> exit("error sending message")
-      end
+      respond_message(Rolls, M_Name, Token),
+      read_message(M_Name, Token)
   end.
 
 respond_message(Rolls, Msg, Token) ->
-  F_Rolls = lists:join(", ", [Roll || [Roll] <- Rolls]),
-  Body = "api_type=json&return_rtjson=false&text="++F_Rolls++"&thing_id="++binary_to_list(Msg),
-  io:fwrite("~s~n", [Body]),
+  F_Rolls = lists:join("%0D%0A", [do_rolls(Roll) || [Roll] <- Rolls]),
+  Body = lists:flatten("api_type=json&return_rtjson=false&text="++F_Rolls++"&thing_id="++binary_to_list(Msg)),
+  io:fwrite("~p~n", [Body]),
   {ok,
   {{"HTTP/1.1",200,"OK"},
     _Content,
@@ -68,7 +64,13 @@ respond_message(Rolls, Msg, Token) ->
                 [{ssl,[]}],
                 []),
   io:fwrite("Result:~p~n", [Result]),
-  Result.
+  case jiffy:decode(Result) of
+    {[{<<"json">>, {[{<<"errors">>, []},_]}}]} -> ok;
+    _ -> 
+       io:fwrite("Sleeping~n"),
+       timer:sleep(600000),
+       loop(Token)
+  end.
 
 read_message(Msg, Token) ->
   Read = "id="++binary_to_list(Msg),
@@ -103,4 +105,9 @@ get_token() ->
     nomatch -> nil;
     Str -> lists:takewhile(fun ($") -> false; (_) -> true end, Str)
   end.
+
+do_rolls([$[|Roll]) ->
+  [N_Dice, Dice_Size] = [element(1, string:to_integer(N)) || N <- string:split(Roll,"d")],
+  Rolls = [rand:uniform(Dice_Size) || _ <- lists:seq(1, N_Dice)],
+  "%2A%2A"++lists:droplast(Roll)++"%3A%2A%2A++%0A%0D"++lists:join("%2C+", [integer_to_list(N) || N <- Rolls])++"++%0A%0D"++"%2A%2ASum%3A%2A%2A+"++integer_to_list(lists:sum(Rolls)). 
 
